39225b4e9e85cec9f468d3567c6d20c262596d30
Kaio Silveira <silveira.kaio@icloud.com>
2023-08-28 15:31:18 +0100

    update client code to be more expressive

--- src/client-code/index.js
+++ src/client-code/index.js
@@ -1,11 +1,12 @@
-/* This code is being adapted based on the original code snippet presented in the book
-so it can be unit tested.
-*/
+import { Priority } from "../priority";
 
+/* This code is being adapted based on the original code snippet presented in the book
+  so it can be unit tested.
+*/
 export function countHighPriorityAndRushOrders(orders) {
-  const highPriorityCount = orders.filter(
-    (o) => "high" === o.priorityString || "rush" === o.priorityString
-  ).length;
+  const highPriorityCount = orders
+    .filter(o => o.priority.higherThan(new Priority('normal')))
+    .length;
 
   return highPriorityCount;
 }
