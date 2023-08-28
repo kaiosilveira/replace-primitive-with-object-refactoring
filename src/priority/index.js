08fa0836f76c9ce1a3ef8d4c5ae70cee6cf0a73f
Kaio Silveira <silveira.kaio@icloud.com>
2023-08-28 15:16:50 +0100

    make Priority class ctor more flexible

--- src/priority/index.js
+++ src/priority/index.js
@@ -1,5 +1,6 @@
 export class Priority {
   constructor(value) {
+    if (value instanceof Priority) return value;
     this._value = value;
   }
 
--- src/priority/index.test.js
+++ src/priority/index.test.js
@@ -5,4 +5,10 @@ describe('Priority', () => {
     const priority = new Priority('high');
     expect(priority.toString()).toEqual('high');
   });
+
+  it('should return the same value if constructor argument is an instance of the Priority class', () => {
+    const existingPriority = new Priority('high');
+    const priority = new Priority(existingPriority);
+    expect(priority).toBe(existingPriority);
+  });
 });
